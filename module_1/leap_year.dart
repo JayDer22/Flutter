ddf28698de45e2d29b0a92e2a845f8e7c6fb1f28
JayDer22 <jdahir203@gmail.com>
2026-03-17 17:25:07 +0530

    22_condition11.dart

--- module_1/leap_year.dart
+++ module_1/leap_year.dart
@@ -2,18 +2,18 @@ import 'dart:io';
 
 void main() {
   print("Enter a year:");
-  int year = int.parse(stdin.readLineSync()!);
+  int year = int.parse(stdin.readLineSync());
 
   if (year % 400 == 0) {
-    print("$year is a Leap Year");
+    print(" Leap Year");
   } else {
     if (year % 100 == 0) {
-      print("$year is Not a Leap Year");
+      print("Not a Leap Year");
     } else {
       if (year % 4 == 0) {
-        print("$year is a Leap Year");
+        print("Leap Year");
       } else {
-        print("$year is Not a Leap Year");
+        print("Not a Leap Year");
       }
     }
   }

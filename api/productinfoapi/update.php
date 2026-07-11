c96f9aa913df0cb0307a91f5a1c502c068a357ef
JayDer22 <jdahir203@gmail.com>
2026-07-11 10:57:58 +0530

    Update update.php

--- api/productinfoapi/update.php
+++ api/productinfoapi/update.php
@@ -6,14 +6,15 @@
     $pprice = $_POST["product_price"];
     $pdes = $_POST["product_des"];
     $pfeaturs = $_POST["product_featurs"];
+    $pimage = $_POST["product_image"];
 
-    if($id==""&& $pname==""&& $pprice==""&& $pdes==""&&$pfeaturs=="")
+    if($id==""&& $pname==""&& $pprice==""&& $pdes==""&&$pfeaturs==""&& $pimag=="")
     {
         echo '0';
     }
     else
     {
-        $sql = "update product_info set product_name='$pname',product_price='$pprice',product_des='$pdes',product_featurs='$pfeaturs' where id='$id'";
+        $sql = "update product_info set product_name='$pname',product_price='$pprice',product_des='$pdes',product_featurs='$pfeaturs',product_image='$pimage' where id='$id'";
         mysqli_query($con,$sql);
     }
     

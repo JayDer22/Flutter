c6f1f8972256dcc515674ad574313b3ff8307020
JayDer22 <jdahir203@gmail.com>
2026-07-14 17:44:21 +0530

    Update insert.php

--- api/productinfoapi/insert.php
+++ api/productinfoapi/insert.php
@@ -5,11 +5,9 @@ $pname = $_POST["product_name"];
 $pprice = $_POST["product_price"];
 $pdes = $_POST["product_des"];
 $pfeaturs = $_POST["product_featurs"];
-$pimage = $_POST["product_image"];
 
 
 $upload_path = 'images/';
-
 $upload_url = 'https://' . $_SERVER['SERVER_NAME'] . "/jay/" . $upload_path;
 
 
@@ -18,21 +16,24 @@ $upload_url = 'https://' . $_SERVER['SERVER_NAME'] . "/jay/" . $upload_path;
 
     $random = 'image_' . rand(1000,9999);
 
-    $file_url = $upload_url . $random . '.' . $extension;
+    $file_url = $upload_url . $random . "." . $extension;
 
-    $file_path = $upload_path . $random . '.' . $extension;
+    $file_path = $upload_path . $random . "." . $extension;
 
     move_uploaded_file($_FILES["product_image"]["tmp_name"], $file_path);
    
 
 
-if($pname=="" && $pprice=="" && $pdes=="" && $pfeaturs=="")
+if($pname=="" || $pprice=="" || $pdes=="" || $pfeaturs=="")
 {
     echo "0";
 }
 else
 {
-    $sql = "INSERT INTO product_info(product_name,product_price,product_des,product_featurs,product_image)VALUES('$pname','$pprice','$pdes','$pfeaturs','$pimage')";
+    $sql = "INSERT INTO product_info
+    (product_name,product_price,product_des,product_featurs,product_image)
+    VALUES
+    ('$pname','$pprice','$pdes','$pfeaturs','$pimage')";
 
     if(mysqli_query($con,$sql))
     {

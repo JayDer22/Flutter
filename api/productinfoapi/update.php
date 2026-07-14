de9fe42fc7b18e8e95493a2db7068e81d9c649ca
JayDer22 <jdahir203@gmail.com>
2026-07-14 17:43:57 +0530

    Update update.php

--- api/productinfoapi/update.php
+++ api/productinfoapi/update.php
@@ -1,47 +1,62 @@
 <?php
-include 'connect.php';
+
+include('connect.php');
+
+$upload_path = "images/";
+$upload_url = "http://".$_SERVER['SERVER_NAME']."/jay/".$upload_path;
 
 $id = $_POST['id'];
 $pname = $_POST['product_name'];
 $pprice = $_POST['product_price'];
 $pdes = $_POST['product_des'];
 $pfeaturs = $_POST['product_featurs'];
-$pimage = $_POST['product_image'];
-
-$upload_path = 'images/';
-
-$upload_url = 'https://' . $_SERVER['SERVER_NAME'] . "/jay/" . $upload_path;
 
 
-    $fileinfo = pathinfo($_FILES["product_image"]["name"]);
-    $extension = $fileinfo["extension"];
-
-    $random = 'image_' . rand(1000, 9999);
-
-    $file_url = $upload_url . $random . '.' . $extension;
-
-    $file_path = $upload_path . $random . '.' . $extension;
-
-    move_uploaded_file($_FILES["product_image"]["tmp_name"], $file_path);
-    $pimage = $file_url;
-    
-
-
-$sql = "UPDATE product_info SET
-product_name='$pname',
-product_price='$pprice',
-product_des='$pdes',
-product_featurs='$pfeaturs',
-product_image='$pimage'
-WHERE id='$id'";
-
-if (mysqli_query($con, $sql))
+if($id=="" || $pname=="" || $pprice=="" || $pdes=="" || $pfeatures=="")
 {
-    echo "1";
+    echo "0";
 }
 else
 {
-    echo mysqli_error($con);
+    if(isset($_FILES["product_image"]) && $_FILES["product_image"]["name"] != "")
+    {
+        $fileinfo = pathinfo($_FILES["product_image"]["name"]);
+        $extension = $fileinfo["extension"];
+
+        $filename = "product_".rand(1000,9999).".".$extension;
+
+        $file_path = $upload_path.$filename;
+        $file_url = $upload_url.$filename;
+
+        move_uploaded_file($_FILES["product_image"]["tmp_name"], $file_path);
+
+        $sql = "UPDATE product_info SET
+         product_name='$pname',
+         product_price='$pprice',
+         product_des='$pdes',
+         product_featurs='$pfeaturs',
+         product_image='$file_url'
+         WHERE id='$id'";
+
+    }
+    else
+    {
+       $sql = "UPDATE product_info SET
+         product_name='$pname',
+         product_price='$pprice',
+         product_des='$pdes',
+         product_featurs='$pfeaturs',
+         WHERE id='$id'";
+    }
+
+    if(mysqli_query($con, $sql))
+    {
+        echo "1";
+    }
+    else
+    {
+        echo "0";
+    }
 }
 
 mysqli_close($con);

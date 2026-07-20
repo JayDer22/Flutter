2e513de42b7ddd6fc80628f0c6497248d8af880b
JayDer22 <jdahir203@gmail.com>
2026-07-20 15:35:37 +0530

    Create fatch_movies2.dart

---
+++ Module-6/Session-2/task-3/fatch_movies2.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class FatchMovies extends StatefulWidget {
+  const FatchMovies({super.key});
+
+  @override
+  State<FatchMovies> createState() => _FatchMoviesState();
+}
+
+class _FatchMoviesState extends State<FatchMovies> {
+  @override
+  void initState() {
+    super.initState();
+    fetchMovies();
+  }
+
+Future<void> fetchMovies() async {
+  final url = Uri.parse('https://api.tvmaze.com/shows');
+
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    List shows = jsonDecode(response.body);
+
+    print("First 10 Shows:");
+
+    for (int i = 0; i < 10; i++) {
+      print(shows[i]['name']);
+    }
+  } else {
+    print('Request failed with status: ${response.statusCode}');
+  }
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Check the console for the API response'),
+      ),
+    );
+  }
+}
